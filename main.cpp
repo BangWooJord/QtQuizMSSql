c663601e03a3bee6a40b1b97fa597c5b2b78887c
BangWooJord <58793833+bangwoojord@users.noreply.github.com>
2021-02-05 19:18:55 +0200

    Remade project structure
    Signed-off-by: BangWooJord <58793833+BangWooJord@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #include <QApplication>
-#include <QPushButton>
-#include "mainwrap.h"
+#include "src/mainwrap.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-//    QPushButton button("Hello world!", nullptr);
-//    button.resize(200, 100);
-//    button.show();
     mainWrap w;
     w.show();
     w.setStyleSheet("background-color: #1c172e;"
